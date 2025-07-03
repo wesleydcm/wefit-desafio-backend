c856aa32a02405ce07c6ed6685ef3fda15914041
wesleydcm <wesleycostamatos@gmail.com>
2025-07-03 11:50:41 -0300

    start repository

---
+++ src/main/java/com/wefit/desafio_backend/WefitDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wefit.desafio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefitDesafioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WefitDesafioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wefit/desafio_backend/WefitDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wefit.desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefitDesafioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
